7cd142a9e4487b2977d942b6c5f551c458b8ac82
lucastinte <lucasrtinte19@gmail.com>
2024-06-21 23:35:05 -0300

    color

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import CV from '../../assets/cv_meri_gogichashvili.pdf';
+import React from "react";
+import CV from "../../assets/cv_meri_gogichashvili.pdf";
 
 const CTA = () => {
   return (
     <div className="cta">
       <a href={CV} download className="btn">
-        Download CV
+        Descargar CV
       </a>
       <a href="#contact" className="btn btn-primary">
-        Let's talk
+        Contacto
       </a>
     </div>
   );
